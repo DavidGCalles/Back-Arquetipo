2981699029e1d4b0e5b9966c609b942065664e83
DavidGCalles <davidgcalles91@gmail.com>
2025-01-20 13:45:52 +0100

    AR-45 AR-50 AR-55
    Modifying rpi ddl to add protocol and object type to pin table

--- app/models/ddl_rpi.sql
+++ app/models/ddl_rpi.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS gpio_pins (
             name TEXT NOT NULL,
             mode TEXT CHECK(mode IN ('INPUT', 'OUTPUT')), 
             state TEXT CHECK(state IN ('HIGH', 'LOW')) DEFAULT NULL,
-            pull TEXT CHECK(pull IN ('PULL_UP', 'PULL_DOWN', 'NONE')) DEFAULT 'NONE'
+            pull TEXT CHECK(pull IN ('PULL_UP', 'PULL_DOWN', 'NONE')) DEFAULT 'NONE',
+            protocol TEXT CHECK(protocol IN ('I2C', 'SPI', 'UART', 'GPIO', 'ONEWIRE')) DEFAULT 'GPIO',
+            object_type TEXT CHECK(object_type IN ('SENSOR', 'ACTUATOR', 'OTHER')) DEFAULT 'OTHER'
         );
