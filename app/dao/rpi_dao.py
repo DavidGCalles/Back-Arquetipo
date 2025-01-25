831442c5930ed49ca5a7b62fa52a8bc2b130660e
DavidGCalles <davidgcalles91@gmail.com>
2025-01-25 18:55:20 +0100

    AR-58 AR-48 AR-45
    Device dao

--- app/dao/rpi_dao.py
+++ app/dao/rpi_dao.py
@@ -46,3 +46,44 @@ class GPIOControlDAO:
     def delete_all(self):
         self.connection.execute("DELETE FROM gpio_pins;")
         self.connection.commit()
+
+class DeviceDAO:
+    def __init__(self):
+        self.db_manager = DBManager()
+        self.db_manager.reset_db_settings("sqlite-rpi")
+        self.connection = DBManager().get_db_connection()
+
+    def insert_device(self, name: str, type: str, manufacturer: str, model: str, serial_number: str, purchase_date: str, warranty_expiration: str, location: str, status: str, pin_number: int, range_min: int, range_max: int, measure_unit: str, last_used: str, value: str):
+        query = """
+        INSERT INTO devices (name, type, manufacturer, model, serial_number, purchase_date, warranty_expiration, location, status, pin_number, range_min, range_max, measure_unit, last_used, value)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        """
+        self.connection.execute(query, (name, type, manufacturer, model, serial_number, purchase_date, warranty_expiration, location, status, pin_number, range_min, range_max, measure_unit, last_used, value))
+        self.connection.commit()
+
+    def update_device(self, device_id: int, **kwargs):
+        updates = []
+        params = []
+
+        for key, value in kwargs.items():
+            updates.append(f"{key} = ?")
+            params.append(value)
+
+        params.append(device_id)
+        query = f"""
+        UPDATE devices
+        SET {', '.join(updates)}
+        WHERE device_id = ?;
+        """
+        self.connection.execute(query, params)
+        self.connection.commit()
+
+    def get_all_devices(self) -> list[dict[str, str]]:
+        query = "SELECT * FROM devices;"
+        cursor = self.connection.execute(query)
+        columns = [column[0] for column in cursor.description]
+        return [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+    def delete_device(self, device_id: int):
+        self.connection.execute("DELETE FROM devices WHERE device_id = ?;", (device_id,))
+        self.connection.commit()
