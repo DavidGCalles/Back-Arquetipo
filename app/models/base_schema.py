34cd09b4236b024e393ed7172c52817621d0e264
DavidGCalles <davidgcalles91@gmail.com>
2025-01-30 23:37:15 +0100

    AR-45 Fix to converter method unification

--- app/models/base_schema.py
+++ app/models/base_schema.py
@@ -4,8 +4,7 @@ class BaseSchema(Schema):
     """
     BaseSchema: Class to manage the schema of the base model.
     """
-    @staticmethod
-    def from_array_to_json(values):
-        keys = BaseSchema().fields.keys()
+    def from_array_to_json(self, values):
+        keys = self.fields.keys()
         return dict(zip(keys, values))
     
