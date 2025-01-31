b4817c7eb9431a5c5b2bc58da90c5c35b79c26e2
DavidGCalles <davidgcalles91@gmail.com>
2025-01-31 02:20:09 +0100

    AR-66 AR-67
    Another way to access sensors

--- app/services/rpi_device_controller.py
+++ app/services/rpi_device_controller.py
@@ -1,10 +1,12 @@
 from w1thermsensor import W1ThermSensor
 from config import LOGGER
+from time import sleep
 
 class DeviceController:
     def __init__(self, data:dict):
         self.data = data
         self.sensor_instance = self.determine_device()
+        LOGGER.info(f"Sensor instance: {self.sensor_instance}")
     def read_device(self):
         if self.sensor_instance is not None and isinstance(self.sensor_instance, W1ThermSensor):
             result = self.sensor_instance.get_temperature("celsius")
@@ -15,4 +17,7 @@ class DeviceController:
         pass
     def determine_device(self):
         if self.data["model"] == "D18B20":
-            return W1ThermSensor(sensor_id=self.data["bus_id"])
+            instances= W1ThermSensor.get_available_sensors()
+            #one line, filter the proper instance by id with bus_id
+            proper_instance = next((x for x in instances if x.id == self.data["bus_id"]), None)
+            return proper_instance
