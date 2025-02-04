4a02f56e6855a064a5af5989b237ee75010f2f00
DavidGCalles <davidgcalles91@gmail.com>
2025-02-04 21:28:27 +0100

    AR-66 AR-69
    Another instantiation placement

--- run.py
+++ run.py
@@ -5,11 +5,9 @@ app = create_app()
 app.secret_key = urandom(24)
 GPIOCONTROLLER = None
 
-if environ.get("RPI_MODULE"):
-    from app.services.rpi_gpio_controller import GPIOController
-    GPIOCONTROLLER = GPIOController()
-
-
 if __name__ == "__main__":
+    if environ.get("RPI_MODULE"):
+        from app.services.rpi_gpio_controller import GPIOController
+        GPIOCONTROLLER = GPIOController()
     port = int(environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port , debug= True)
